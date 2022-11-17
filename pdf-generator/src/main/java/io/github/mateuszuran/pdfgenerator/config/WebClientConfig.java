aebb4dfa56163760c990fb1455d0f677679bc9f5
mateusz-uran <mateusz.uranowski@onet.pl>
2022-11-17 14:22:32 +0100

    fix: load static resources

--- pdf-generator/src/main/java/io/github/mateuszuran/pdfgenerator/config/WebClientConfig.java
+++ pdf-generator/src/main/java/io/github/mateuszuran/pdfgenerator/config/WebClientConfig.java
@@ -3,7 +3,11 @@ package io.github.mateuszuran.pdfgenerator.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class WebClientConfig {
@@ -12,5 +16,10 @@ public class WebClientConfig {
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> cssRouter() {
+        return RouterFunctions.resources("/css/**", new ClassPathResource("css/"));
     }
 }
