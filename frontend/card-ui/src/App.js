d660f12cbaa0bb14a5b21e36bdfef085cad16229
mateusz-uran <mateusz.uranowski@onet.pl>
2022-11-30 11:49:28 +0100

    feat: add dynamic inputs for trip form and validation if cardId is null

--- frontend/card-ui/src/App.js
+++ frontend/card-ui/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Card from './components/Card';
+import AddTrip from './components/AddTrip';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Routes>
+        <Route path='/' element={<Card />} />
+        <Route path='/add-trip' element={<AddTrip />} />
+      </Routes>
     </div>
   );
 }
--- frontend/card-ui/src/components/AddTrip.js
+++ frontend/card-ui/src/components/AddTrip.js
@@ -1,10 +1,79 @@
-import React from 'react';
-import { useNavigate } from "react-router-dom"
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 
-function AddTrip(props) {
+function AddTrip() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    let cardId = location.state.cardId;
+    const submitHandler = async () => {
+        try {
+            navigate(-1); // Omit optional second argument
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    const [inputFields, setInputFields] = useState([
+        { name: '', age: '' }
+    ])
+
+    const [prev, setPrev] = useState('');
+
+    const handleFormChange = (index, event) => {
+        let data = [...inputFields];
+        data[index][event.target.name] = event.target.value;
+        setInputFields(data);
+        setPrev(event.target.value);
+    }
+
+    const addFields = () => {
+        let newField = { name: prev, age: '' }
+        setInputFields([...inputFields, newField])
+    }
+
+    const removeFields = (index) => {
+        let data = [...inputFields];
+        data.splice(index, 1)
+        setInputFields(data)
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(inputFields)
+    }
+
+    useEffect(() => {
+        console.log("from trip: " + cardId);
+    }, [])
+
     return (
         <div>
             <h1>Trips</h1>
+            <button onClick={() => submitHandler()}>back</button>
+            <form onSubmit={submit}>
+                {inputFields.map((input, index) => {
+                    return (
+                        <div key={index}>
+                            <input
+                                name='name'
+                                placeholder='Name'
+                                value={input.name}
+                                onChange={event => handleFormChange(index, event)}
+                            />
+                            <input
+                                name='age'
+                                placeholder='Age'
+                                value={input.age}
+                                onChange={event => handleFormChange(index, event)}
+                            />
+                            <button onClick={() => removeFields(index)}>more</button>
+                        </div>
+                    )
+                })}
+            </form>
+            <button onClick={addFields}>more</button>
+            <button onClick={submit}>Submit</button>
         </div>
     );
 }
