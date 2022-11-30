0e19474748df3843a3cd41b3ca0fdefb418b45d8
mateusz-uran <mateusz.uranowski@onet.pl>
2022-11-30 08:59:23 +0100

    refactor: add App.js as root component and animate showing form for adding card

--- frontend/card-ui/src/index.js
+++ frontend/card-ui/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Card from './components/Card';
 import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Card/>
+    <App />
   </React.StrictMode>
 );
