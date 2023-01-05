f54472b0ee7a77fb0ff674c183bd57c4e374ea64
mateusz-uran <mateusz.uranowski@onet.pl>
2023-01-05 21:03:59 +0100

    fix: tracing api gateway and recreate docker image with new configuration

--- user/src/main/java/io/github/mateuszuran/user/config/SecurityConfiguration.java
+++ user/src/main/java/io/github/mateuszuran/user/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package io.github.mateuszuran.user.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
