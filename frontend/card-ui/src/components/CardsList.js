a4880337fb74df87f7222a0ac988abaa4a4d37bb
mateusz-uran <mateusz.uranowski@onet.pl>
2023-03-12 20:56:29 +0100

    feat: replace useState form card submit with useFormik and add yup validations

--- frontend/card-ui/src/components/CardsList.js
+++ frontend/card-ui/src/components/CardsList.js
@@ -13,6 +13,8 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link, Outlet } from 'react-router-dom';
 import CheckIcon from '@mui/icons-material/Check';
+import { useFormik } from 'formik';
+import * as yup from "yup";
 
 function CardsList(props) {
     const { user, mode } = props;
@@ -22,32 +24,38 @@ function CardsList(props) {
     const day = getCurrentDay();
 
     const [cardsList, setCardsList] = useState([]);
-    const [number, setNumber] = useState("");
 
     const [cardId, setCardId] = useState(0);
     const [renderCardInfoHandler, setRenderCardInfoHandler] = useState(false);
 
-    function retrieveCardByUserAndDate() {
+    const retrieveCardByUserAndDate = () => {
         CardService.getCardByUserAndMonth(user, year, month)
             .then(response => {
                 setCardsList(response.data);
             });
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        let cardPayload = {
-            number: number,
-            authorUsername: user
-        }
-        CardService.create(cardPayload, year, month, day)
-            .then(response => {
-                setCardsList(cardsList => [...cardsList, response.data]);
-                setNumber('');
-            }, (error) => {
-                console.log(error)
-            })
-    }
+    const formik = useFormik({
+        initialValues: {
+            number: '',
+        },
+        validationSchema: yup.object({
+            number: yup.string().required("Hour start cannot be empty"),
+        }),
+        onSubmit: (values, { resetForm }) => {
+            let cardPayload = {
+                number: values.number,
+                authorUsername: user
+            }
+            CardService.create(cardPayload, year, month, day)
+                .then(response => {
+                    setCardsList(cardsList => [...cardsList, response.data]);
+                    resetForm();
+                }, (error) => {
+                    console.log(error)
+                })
+        },
+    });
 
     const handleFinishCard = (id) => {
         CardService.toggleCard(id)
@@ -58,7 +66,7 @@ function CardsList(props) {
             })
     }
 
-    function toggleSelectedCard(id) {
+    const toggleSelectedCard = (id) => {
         setCardsList(prevState => {
             const updatedCards = prevState.map(card => {
                 if (card.id === id) {
@@ -89,7 +97,7 @@ function CardsList(props) {
             })
     }
 
-    function checkStorage() {
+    const checkStorage = () => {
         let storedCardId = JSON.parse(localStorage.getItem('selectedCard'));
         if (storedCardId !== undefined && storedCardId !== null) {
             setCardId(storedCardId);
@@ -105,18 +113,19 @@ function CardsList(props) {
     return (
         <div className={`flex lg:flex-row flex-col px-4 ${mode ? 'text-white' : ''}`}>
             <div className='lg:w-1/6 my-2'>
-                <form>
+                <form onSubmit={formik.handleSubmit}>
                     <div className='flex items-center'>
                         <TextField
-                            className={mode ? 'text-white' : ''}
-                            id="outlined-basic"
-                            label="Number"
-                            value={number}
-                            onChange={(event) => {
-                                setNumber(event.target.value);
-                            }}
+                            id="number"
+                            name="number"
+                            label="number"
+                            value={formik.values.number}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            error={formik.touched.number && Boolean(formik.errors.number)}
+                            helperText={formik.touched.number && formik.errors.number}
                         />
-                        <IconButton onClick={handleSubmit}>
+                        <IconButton type="submit">
                             <AddIcon className={mode ? 'text-white' : ''} />
                         </IconButton>
                     </div>
